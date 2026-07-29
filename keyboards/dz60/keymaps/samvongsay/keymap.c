3c541215f163c53f040f4c1a85844a7fa43f39e6
samvongsay <sdvongsay@gmail.com>
2026-07-29 16:17:57 -0700

    Add grave to dz60

--- keyboards/dz60/keymaps/samvongsay/keymap.c
+++ keyboards/dz60/keymaps/samvongsay/keymap.c
@@ -23,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
   /* Layer 1
    * ,-----------------------------------------------------------------------------------------.
-   * | ESC  | F1 |  F2  | F3 |  F4  |  F5  |  F6  |  F7  |  F8   | F9 | F10 | F11 | F12 | Bkspc|
+   * |   `  | F1 |  F2  | F3 |  F4  |  F5  |  F6  |  F7  |  F8   | F9 | F10 | F11 | F12 | Bkspc|
    * |-----------------------------------------------------------------------------------------+
-   * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  7  |  8  |  9  |  P  |  [  |  ]  |    \   |
+   * | Tab    |  Q  |  U  |  E  |  R  |  T  |  Y  |  7  |  8  |  9  |  P  |  [  |  ]  |    \   |
    * |-----------------------------------------------------------------------------------------+
-   * | CAPS    |  A  |  S  |  D  |  F  |  G  |  H  |  4  |  5  |  6  |  ;  |  '  |    Enter    |
+   * | CAPS    |  L  |  D  |  R  |  F  |  G  |  H  |  4  |  5  |  6  |  ;  |  '  |    Enter    |
    * |-----------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  1  |  2  |  3  |  /  | INS | HOME| PGUP|
    * |-----------------------------------------------------------------------------------------+
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------------'
    */
     LAYOUT_all(
-        KC_GESC,	KC_F1,   	KC_F2,    	KC_F3,	    KC_F4,    	KC_F5,    	KC_F6,    	KC_F7,    	KC_F8,    	KC_F9,    	KC_F10,    	KC_F11,  	KC_F12,  	XXXXXXX, 	KC_BSPC,
-        KC_TAB,  				KC_Q,   	KC_W,    	KC_E,    	KC_R,    	KC_T,    	KC_Y,    	KC_7,    	KC_8,    	KC_9,    	KC_P,    	KC_LBRC, 	KC_RBRC,	KC_BSLS, 
-        KC_CAPS, 				KC_A,   	KC_S,    	KC_D,    	KC_F,    	KC_G,    	KC_H,    	KC_4,    	KC_5,    	KC_6,    	KC_SCLN, 	KC_QUOT, 	KC_ENT, 
+        KC_GRAVE,   KC_F1,   	KC_F2,    	KC_F3,	    KC_F4,    	KC_F5,    	KC_F6,    	KC_F7,    	KC_F8,    	KC_F9,    	KC_F10,    	KC_F11,  	KC_F12,  	XXXXXXX, 	KC_BSPC,
+        KC_TAB,  				KC_Q,   	KC_UP,    	KC_E,    	KC_R,    	KC_T,    	KC_Y,    	KC_7,    	KC_8,    	KC_9,    	KC_P,    	KC_LBRC, 	KC_RBRC,	KC_BSLS, 
+        KC_CAPS, 				KC_LEFT,   	KC_DOWN,    KC_RIGHT,  	KC_F,    	KC_G,    	KC_H,    	KC_4,    	KC_5,    	KC_6,    	KC_SCLN, 	KC_QUOT, 	KC_ENT, 
         KC_LSFT, 	KC_LSFT,	KC_Z,    	KC_X,    	KC_C,    	KC_V,    	KC_B,    	KC_N,    	KC_1,    	KC_2,    	KC_3,    	KC_SLSH, 	KC_INS,  	KC_HOME, 	KC_PGUP,
         KC_LCTL, 	KC_LGUI, 				KC_LALT,   	KC_SPC,  				MO(1),					KC_0,      				KC_RALT, 	KC_RCTL, 	KC_DEL,  	KC_END,  	KC_PGDN
     ),
